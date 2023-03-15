8410c852616a2171d78aed2021743cd8028cd529
zikriwahyu1234 <royybang14@gmail.com>
2023-03-15 14:37:59 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,126 @@
+<!--
+=========================================================
+* Material Kit 2 - v3.0.4
+=========================================================
+
+* Product Page:  https://www.creative-tim.com/product/material-kit 
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+<link rel="icon" type="image/png" href="./assets/img/favicon.png">
+<title>
+  Material Kit 2 by Creative Tim
+</title>
+<!--     Fonts and icons     -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+
+<!-- Nucleo Icons -->
+<link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+<link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+
+<!-- Font Awesome Icons -->
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+
+<!-- CSS Files -->
+<link id="pagestyle" href="./assets/css/material-kit.css?v=3.0.4" rel="stylesheet" />
+</head>
+<body class="index-page bg-gray-200">  
+  <!-- Navbar -->
+<div class="container position-sticky z-index-sticky top-0"><div class="row"><div class="col-12">
+<nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+  <div class="container-fluid px-0">
+    <a class="navbar-brand font-weight-bolder ms-sm-3" href="https://www.instagram.com/_bangajit25/" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+      My Profile
+    </a>
+    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon mt-2">
+        <span class="navbar-toggler-bar bar1"></span>
+        <span class="navbar-toggler-bar bar2"></span>
+        <span class="navbar-toggler-bar bar3"></span>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
+      <ul class="navbar-nav navbar-nav-hover ms-auto">
+        <li class="nav-item dropdown dropdown-hover mx-2">
+          <a class="nav-link ps-2 d-flex cursor-pointer align-items-center" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="material-icons opacity-6 me-2 text-md">dashboard</i>
+            Pages
+            <img src="./assets/img/down-arrow-dark.svg" alt="down-arrow" class="arrow ms-auto ms-md-2">
+          </a>
+          <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3" aria-labelledby="dropdownMenuPages">
+            <div class="d-none d-lg-block">
+  <h6 class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
+    Landing Pages
+  </h6>
+  <a href="./pages/about-us.html" class="dropdown-item border-radius-md">
+    <span>About Us</span>
+  </a>
+  <a href="./pages/contact-us.html" class="dropdown-item border-radius-md">
+    <span>Contact Us</span>
+  </a>
+  <a href="./pages/author.html" class="dropdown-item border-radius-md">
+    <span>Author</span>
+  </a>
+</div>
+
+<div class="d-lg-none">
+  <h6 class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
+    Landing Pages
+  </h6>
+
+  <a href="./pages/about-us.html" class="dropdown-item border-radius-md">
+    <span>About Us</span>
+  </a>
+  <a href="./pages/contact-us.html" class="dropdown-item border-radius-md">
+    <span>Contact Us</span>
+  </a>
+  <a href="./pages/author.html" class="dropdown-item border-radius-md">
+    <span>Author</span>
+  </a>
+</div>
+</div>
+</li>
+       </li>
+        <li class="nav-item ms-lg-auto">
+          <a class="nav-link nav-link-icon me-2" href="https://github.com/zikriwahyu1234" target="_blank">
+            <i class="fa fa-github me-1"></i>
+            <p class="d-inline text-sm z-index-1 font-weight-bold" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Star us on Github">Github</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+</div></div></div>
+<header class="header-2">
+  <div class="page-header min-vh-75 relative" style="background-image: url('./assets/img/bg87.jpg')">
+    <span class="mask bg-gradient-black opacity-4"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto">
+          <h1 class="text-white pt-3 mt-n5">Welcome My Profile</h1>
+          <p class="lead text-white mt-3">Zikri Wahyu <br/> Politeknik Negeri Padang </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+</body>
+
+</html>
